3ad355ba7f5ca979f50d29d396bd0a218b2392a4
ligi <ligi@ligi.de>
2013-02-08 01:38:24 +0100

    move the temp dir

--- src/org/ligi/ticketviewer/TicketDefinitions.java
+++ src/org/ligi/ticketviewer/TicketDefinitions.java
@@ -3,13 +3,13 @@ package org.ligi.ticketviewer;
 import android.content.Context;
 
 public class TicketDefinitions {
-	
-	public static String getPassesDir(Context ctx) {
-		return ctx.getFilesDir().getAbsolutePath()+"/passes";
-	}
-	
 
-	public static String getTmpDir(Context ctx) {
-		return ctx.getFilesDir().getAbsolutePath()+"/passes/tmp/";
-	}
+    public static String getPassesDir(Context ctx) {
+        return ctx.getFilesDir().getAbsolutePath() + "/passes";
+    }
+
+
+    public static String getTmpDir(Context ctx) {
+        return ctx.getFilesDir().getAbsolutePath() + "/tmp/passes";
+    }
 }
