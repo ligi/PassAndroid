471dd684a79782b1ff1350de1380edd73e1a639c
ligi <ligi@ligi.de>
2013-02-10 14:59:57 +0100

    we don't have to set the context here - is set in Application Controller

--- src/org/ligi/ticketviewer/FullscreenImageActivity.java
+++ src/org/ligi/ticketviewer/FullscreenImageActivity.java
@@ -2,7 +2,6 @@ package org.ligi.ticketviewer;
 
 import android.os.Bundle;
 import android.widget.ImageView;
-import com.google.analytics.tracking.android.EasyTracker;
 
 public class FullscreenImageActivity extends TicketViewActivityBase {
 
@@ -10,15 +9,10 @@ public class FullscreenImageActivity extends TicketViewActivityBase {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        EasyTracker.getInstance().setContext(this);
-
         setContentView(R.layout.fullscreen_image);
         ImageView iv = (ImageView) findViewById(R.id.fullscreen_image);
 
-
         iv.setImageBitmap(passbookParser.getBarcodeBitmap());
-
     }
-
 
 }
