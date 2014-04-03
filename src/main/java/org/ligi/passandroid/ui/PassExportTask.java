ef818c6c281bfee053167afb492fa9890382879e
ligi <ligi@ligi.de>
2014-04-03 15:55:08 +0530

    improve export code

--- src/main/java/org/ligi/passandroid/ui/PassExportTask.java
+++ src/main/java/org/ligi/passandroid/ui/PassExportTask.java
@@ -5,6 +5,10 @@ import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
+import android.widget.Toast;
+
+import org.ligi.passandroid.R;
+import org.ligi.passandroid.Tracker;
 
 import java.io.File;
 import java.io.FileInputStream;
@@ -15,29 +19,78 @@ import java.util.zip.ZipOutputStream;
 
 class PassExportTask extends AsyncTask<Void, Void, Void> {
 
-    private ProgressDialog progress_dialog;
-    protected Context ctx;
-    private String path;
-    protected String zipFileName, zipPath;
-    private boolean share_after_export;
+    private final ProgressDialog progress_dialog;
+    protected final Context ctx;
+    private final String inputPath;
+    protected final String zipFileName, zipPath;
 
-    public PassExportTask(Context ctx, String path, String zip_path, String zip_fname, boolean share_after_export) {
+    private boolean share_after_export;
+    private Exception exception;
+
+    public PassExportTask(final Context ctx,
+                          final String inputPath,
+                          final String zipPath,
+                          final String zipFileName,
+                          final boolean share_after_export) {
         super();
         this.ctx = ctx;
-        this.path = path;
-        this.zipFileName = zip_fname;
-        this.zipPath = zip_path;
+        this.inputPath = inputPath;
+        this.zipFileName = zipFileName;
+        this.zipPath = zipPath;
 
         this.share_after_export = share_after_export;
+        progress_dialog = new ProgressDialog(ctx);
     }
 
     @Override
     protected void onPreExecute() {
-        progress_dialog = new ProgressDialog(ctx);
-        progress_dialog.setTitle("Preparing Pass");
-        progress_dialog.setMessage("please wait - shouldn't be long");
+        progress_dialog.setTitle(ctx.getString(R.string.preparing_pass));
+        progress_dialog.setMessage(ctx.getString(R.string.please_wait));
         progress_dialog.show();
         super.onPreExecute();
+    }
+
+
+    @Override
+    protected Void doInBackground(Void... params) {
+        byte[] buf = new byte[1024];
+
+        try {
+            final File zipPathFile = new File(zipPath);
+            zipPathFile.mkdirs();
+
+            final String zipFullFilename = zipPath + "/" + zipFileName;
+
+            // Create the ZIP file
+            ZipOutputStream out = new ZipOutputStream(new FileOutputStream(zipFullFilename));
+            // Compress the files
+
+            final File[] files = new File(inputPath).listFiles();
+
+            if (files == null) {
+                throw new IllegalStateException("cannot listFiles in InputPath - is null");
+            }
+
+            for (File filename : files) {
+                final FileInputStream in = new FileInputStream(filename);
+                // Add ZIP entry to output stream.
+                out.putNextEntry(new ZipEntry(filename.getName()));
+                // Transfer bytes from the file to the ZIP file
+                int len;
+                while ((len = in.read(buf)) > 0) {
+                    out.write(buf, 0, len);
+                }
+                out.closeEntry();
+                in.close();
+            }
+            out.close();
+        } catch (IOException | IllegalStateException exception) {
+            Tracker.get().trackException("when exporting pass to zip", exception, false);
+            this.exception = exception; // we need to take action on the main thread later
+            new File(zipFileName).delete(); // prevent zombies from taking over
+        }
+
+        return null;
     }
 
     @Override
@@ -48,52 +101,18 @@ class PassExportTask extends AsyncTask<Void, Void, Void> {
             progress_dialog.dismiss();
         }
 
+        if (exception != null) {
+            Toast.makeText(ctx, "could not export pass " + exception, Toast.LENGTH_LONG).show();
+            return;
+        }
+
         if (share_after_export) {
             Intent it = new Intent(Intent.ACTION_SEND);
-            it.putExtra(Intent.EXTRA_SUBJECT, "a Passbook is shared with you");
+            it.putExtra(Intent.EXTRA_SUBJECT, ctx.getString(R.string.passbook_is_shared_subject));
             it.putExtra(Intent.EXTRA_STREAM, Uri.parse("file://" + zipPath + zipFileName));
             it.setType("application/vnd.apple.pkpass");
-            ctx.startActivity(Intent.createChooser(it, "How to send Pass?"));
+            ctx.startActivity(Intent.createChooser(it, ctx.getString(R.string.passbook_share_chooser_title)));
         }
     }
 
-    @Override
-    protected Void doInBackground(Void... params) {
-        byte[] buf = new byte[1024];
-
-        try {
-            String destinationDir = zipPath;
-
-            boolean result = (new File(destinationDir)).mkdirs();
-
-            String zipFullFilename = destinationDir + "/" + zipFileName;
-
-            System.out.println(result);
-
-            // Create the ZIP file
-            ZipOutputStream out = new ZipOutputStream(new FileOutputStream(zipFullFilename));
-            // Compress the files
-
-            File[] files = new File(path).listFiles();
-            for (File filename : files) {
-                FileInputStream in = new FileInputStream(filename);
-                // Add ZIP entry to output stream.
-                out.putNextEntry(new ZipEntry(filename.getName()));
-                // Transfer bytes from the file to the ZIP file
-                int len;
-                while ((len = in.read(buf)) > 0) {
-                    out.write(buf, 0, len);
-                }
-                // Complete the entry
-                out.closeEntry();
-                in.close();
-            } // Complete the ZIP file
-            out.close();
-
-        } catch (IOException e) {
-        }
-
-
-        return null;
-    }
 }
