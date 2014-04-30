7ce88fc2473f774265e9135a30f0a09956359ca7
Lukas Niemeier <lukas.niemeier@student.hpi.uni-potsdam.de>
2014-04-30 13:14:22 +0200

    fix for scroll up vs. refresh

--- src/main/java/org/ligi/passandroid/ui/TicketListActivity.java
+++ src/main/java/org/ligi/passandroid/ui/TicketListActivity.java
@@ -21,6 +21,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.Window;
+import android.widget.AbsListView;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
@@ -184,6 +185,18 @@ public class TicketListActivity extends ActionBarActivity {
 
                 return true;
 
+            }
+        });
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) {
+                // nothing here
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                int topPosition = listView.getChildCount() == 0 ? 0 : listView.getChildAt(0).getTop();
+                swipeRefreshLayout.setEnabled(topPosition >= 0);
             }
         });
 
