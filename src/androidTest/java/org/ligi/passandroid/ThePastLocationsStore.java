ee0ade40509c104d5b8768d65f8113d4a138294b
ligi <ligi@ligi.de>
2014-07-06 22:16:24 +0200

    add tests for the pastLocationsStore

---
+++ src/androidTest/java/org/ligi/passandroid/ThePastLocationsStore.java
@@ -0,0 +1,40 @@
+package org.ligi.passandroid;
+
+import android.app.Activity;
+import android.test.suitebuilder.annotation.SmallTest;
+
+import org.ligi.passandroid.model.PastLocationsStore;
+
+import static org.fest.assertions.api.Assertions.assertThat;
+
+public class ThePastLocationsStore extends BaseIntegration<Activity> {
+
+    public ThePastLocationsStore() {
+        super(Activity.class);
+    }
+
+    @SmallTest
+    public void testPastLocationsStoreShouldNeverContainMoreThanMaxElements() {
+        PastLocationsStore tested = new PastLocationsStore(getInstrumentation().getContext());
+
+        for (int i = 0; i < PastLocationsStore.MAX_ELEMENTS * 2; i++) {
+            tested.putLocation("" + i);
+        }
+
+        assertThat(tested.getLocations().size()).isLessThan(PastLocationsStore.MAX_ELEMENTS);
+
+    }
+
+    @SmallTest
+    public void testPastLocationsStoreShouldStoreOnlyOneOfAKind() {
+        PastLocationsStore tested = new PastLocationsStore(getInstrumentation().getContext());
+
+        for (int i = 0; i < 3; i++) {
+            tested.putLocation("foo");
+        }
+
+        assertThat(tested.getLocations().size()).isLessThan(2);
+
+    }
+
+}
