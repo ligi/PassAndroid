a6d9d0305ca5321a29022d6e8088cd0e7a946dbb
ligi <ligi@ligi.de>
2014-07-16 21:45:58 +0200

    invert if conditions for better readability

--- src/main/java/org/ligi/passandroid/model/ApplePassbookQuirkCorrector.java
+++ src/main/java/org/ligi/passandroid/model/ApplePassbookQuirkCorrector.java
@@ -25,65 +25,69 @@ public class ApplePassbookQuirkCorrector {
     }
 
     private static void careForWestbahn(PassImpl pass) {
-        if (!pass.getRelevantDate().isPresent() &&
-                pass.getOrganisation().isPresent() &&
-                pass.getOrganisation().get().equals("WESTbahn")
+        if (pass.getRelevantDate().isPresent() ||
+                !pass.getOrganisation().isPresent() ||
+                !pass.getOrganisation().get().equals("WESTbahn")
                 ) {
-
-            final Optional<PassField> originField = pass.getPrimaryFields().getPassFieldForKey("from");
-            final Optional<PassField> destinationField = pass.getPrimaryFields().getPassFieldForKey("to");
-
-            String description = "WESTbahn";
-
-            if (originField.isPresent()) {
-                description = originField.get().value;
-            }
-
-            if (destinationField.isPresent()) {
-                description += "->" + destinationField.get().value;
-            }
-
-            pass.setDescription(description);
+            return;
         }
+
+        final Optional<PassField> originField = pass.getPrimaryFields().getPassFieldForKey("from");
+        final Optional<PassField> destinationField = pass.getPrimaryFields().getPassFieldForKey("to");
+
+        String description = "WESTbahn";
+
+        if (originField.isPresent()) {
+            description = originField.get().value;
+        }
+
+        if (destinationField.isPresent()) {
+            description += "->" + destinationField.get().value;
+        }
+
+        pass.setDescription(description);
     }
 
     private static void careForTUIFlight(PassImpl pass) {
-        if (pass.getDescription().equals("TUIfly pass")) {
-            final Optional<PassField> originField = pass.getPrimaryFields().getPassFieldForKey("Origin");
-            final Optional<PassField> destinationField = pass.getPrimaryFields().getPassFieldForKey("Des");
-            final Optional<PassField> seatField = pass.getAuxiliaryFields().getPassFieldForKey("SeatNumber");
+        if (!pass.getDescription().equals("TUIfly pass")) {
+            return;
+        }
 
-            if (originField.isPresent() && destinationField.isPresent()) {
-                String description = originField.get().value + "->" + destinationField.get().value;
-                if (seatField.isPresent()) {
-                    description += " @" + seatField.get().value;
-                }
-                pass.setDescription(description);
+        final Optional<PassField> originField = pass.getPrimaryFields().getPassFieldForKey("Origin");
+        final Optional<PassField> destinationField = pass.getPrimaryFields().getPassFieldForKey("Des");
+        final Optional<PassField> seatField = pass.getAuxiliaryFields().getPassFieldForKey("SeatNumber");
+
+        if (originField.isPresent() && destinationField.isPresent()) {
+            String description = originField.get().value + "->" + destinationField.get().value;
+            if (seatField.isPresent()) {
+                description += " @" + seatField.get().value;
             }
+            pass.setDescription(description);
         }
     }
 
 
     private static void careForAirBerlin(PassImpl pass) {
-        if (pass.getDescription().equals("boardcard")) {
-            final String flightRegex = "\\b\\w{1,3}\\d{3,4}\\b";
-
-            Optional<PassField> flightField = pass.getAuxiliaryFields().getPassFieldThatMatchesLabel(flightRegex);
-
-
-            if (!flightField.isPresent()) {
-                flightField = pass.getHeaderFields().getPassFieldThatMatchesLabel(flightRegex);
-            }
-
-            final Optional<PassField> seatField = pass.getAuxiliaryFields().getPassFieldForKey("seat");
-            final Optional<PassField> boardingGroupField = pass.getSecondaryFields().getPassFieldForKey("boardingGroup");
-
-            if (flightField.isPresent() && seatField.isPresent() && boardingGroupField.isPresent()) {
-                String description = flightField.get().label + " " + flightField.get().value;
-                description += " | " + seatField.get().label + " " + seatField.get().value;
-                description += " | " + boardingGroupField.get().label + " " + boardingGroupField.get().value;
-                pass.setDescription(description);
-            } // otherwise fallback to default - better save than sorry
+        if (!pass.getDescription().equals("boardcard")) {
+            return;
         }
+
+        final String flightRegex = "\\b\\w{1,3}\\d{3,4}\\b";
+
+        Optional<PassField> flightField = pass.getAuxiliaryFields().getPassFieldThatMatchesLabel(flightRegex);
+
+        if (!flightField.isPresent()) {
+            flightField = pass.getHeaderFields().getPassFieldThatMatchesLabel(flightRegex);
+        }
+
+        final Optional<PassField> seatField = pass.getAuxiliaryFields().getPassFieldForKey("seat");
+        final Optional<PassField> boardingGroupField = pass.getSecondaryFields().getPassFieldForKey("boardingGroup");
+
+        if (flightField.isPresent() && seatField.isPresent() && boardingGroupField.isPresent()) {
+            String description = flightField.get().label + " " + flightField.get().value;
+            description += " | " + seatField.get().label + " " + seatField.get().value;
+            description += " | " + boardingGroupField.get().label + " " + boardingGroupField.get().value;
+            pass.setDescription(description);
+        } // otherwise fallback to default - better save than sorry
     }
 }
