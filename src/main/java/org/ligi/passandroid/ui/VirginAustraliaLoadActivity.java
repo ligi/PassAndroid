66803618142df892e65f2bb16ee7294761406a0a
ligi <ligi@ligi.de>
2014-07-18 13:57:35 +0200

    Workaround for server block of Virgin Australia

---
+++ src/main/java/org/ligi/passandroid/ui/VirginAustraliaLoadActivity.java
@@ -0,0 +1,27 @@
+package org.ligi.passandroid.ui;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+
+import org.ligi.passandroid.Tracker;
+
+import java.net.URLEncoder;
+
+public class VirginAustraliaLoadActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        final String passId = getIntent().getData().getQueryParameter("c");
+        final String url = "https://mobile.virginaustralia.com/boarding/pass.pkpass?key=" + URLEncoder.encode(passId);
+
+        Tracker.get().trackEvent("quirk_fix", "redirect", "virgin_australia", null);
+        final Intent intent = new Intent(this, PassImportActivity.class);
+        intent.setData(Uri.parse(url));
+        startActivity(intent);
+        finish();
+    }
+}
