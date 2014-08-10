7e8bec7c5e3ad0cc215f9b72463782a7702a0b6f
ligi <ligi@ligi.de>
2014-08-10 13:47:13 +0200

    add missing import

--- src/androidTest/java/org/ligi/passandroid/ThePassViewActivity.java
+++ src/androidTest/java/org/ligi/passandroid/ThePassViewActivity.java
@@ -17,8 +17,8 @@ import static com.google.android.apps.common.testing.ui.espresso.Espresso.onView
 import static com.google.android.apps.common.testing.ui.espresso.action.ViewActions.click;
 import static com.google.android.apps.common.testing.ui.espresso.assertion.ViewAssertions.matches;
 import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.isDisplayed;
+import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withId;
 import static com.google.android.apps.common.testing.ui.espresso.matcher.ViewMatchers.withText;
-import static org.hamcrest.CoreMatchers.not;
 
 @TargetApi(14)
 public class ThePassViewActivity extends BaseIntegration<PassViewActivity> {
