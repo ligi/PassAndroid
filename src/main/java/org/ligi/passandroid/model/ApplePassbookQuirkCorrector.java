852ed70fafcd91420c615f05a15267ead9ddb233
ligi <ligi@ligi.de>
2015-01-28 20:12:02 +0100

    Fix indent

--- src/main/java/org/ligi/passandroid/model/ApplePassbookQuirkCorrector.java
+++ src/main/java/org/ligi/passandroid/model/ApplePassbookQuirkCorrector.java
@@ -6,7 +6,7 @@ import org.ligi.passandroid.Tracker;
 
 public class ApplePassbookQuirkCorrector {
 
-    public static void correctQuirks(PassImpl pass ) {
+    public static void correctQuirks(PassImpl pass) {
         careForTUIFlight(pass);
         careForAirBerlin(pass);
         careForWestbahn(pass);
@@ -17,22 +17,22 @@ public class ApplePassbookQuirkCorrector {
         careForSWISS(pass);
         careForRenfe(pass);
     }
-    
+
     private static void careForRenfe(PassImpl pass) {
-    	if (!pass.getOrganisation().isPresent() || !pass.getOrganisation().get().equals("RENFE OPERADORA")) {
-    		return;
-    	}
-    	Tracker.get().trackEvent("quirk_fix", "description_replace", "RENFE OPERADORA", 0L);
-    	
-    	final Optional<PassField> optionalDepart = pass.getPrimaryFields().getPassFieldForKey("boardingTime");
-    	final Optional<PassField> optionalArrive = pass.getPrimaryFields().getPassFieldForKey("destino");
-    	
-    	if (optionalDepart.isPresent() && optionalArrive.isPresent()) {
-    		Tracker.get().trackEvent("quirk_fix", "description_replace", "RENFE OPERADORA", 1L);
-    		pass.setDescription(optionalDepart.get().label + " -> " + optionalArrive.get().label);
-    	}
+        if (!pass.getOrganisation().isPresent() || !pass.getOrganisation().get().equals("RENFE OPERADORA")) {
+            return;
+        }
+        Tracker.get().trackEvent("quirk_fix", "description_replace", "RENFE OPERADORA", 0L);
+
+        final Optional<PassField> optionalDepart = pass.getPrimaryFields().getPassFieldForKey("boardingTime");
+        final Optional<PassField> optionalArrive = pass.getPrimaryFields().getPassFieldForKey("destino");
+
+        if (optionalDepart.isPresent() && optionalArrive.isPresent()) {
+            Tracker.get().trackEvent("quirk_fix", "description_replace", "RENFE OPERADORA", 1L);
+            pass.setDescription(optionalDepart.get().label + " -> " + optionalArrive.get().label);
+        }
     }
-    
+
     private static void careForSWISS(PassImpl pass) {
         if (!pass.getOrganisation().isPresent() || !pass.getOrganisation().get().equals("SWISS")) {
             return;
