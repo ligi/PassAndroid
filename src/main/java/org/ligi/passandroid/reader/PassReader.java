e8ef2c5d518c8a9cf8cd1f75171f094fea3e1d41
ligi <ligi@ligi.de>
2015-03-05 23:37:34 +0100

    Also read barcode

--- src/main/java/org/ligi/passandroid/reader/PassReader.java
+++ src/main/java/org/ligi/passandroid/reader/PassReader.java
@@ -1,15 +1,15 @@
 package org.ligi.passandroid.reader;
 
 import android.graphics.Color;
-
+import com.google.zxing.BarcodeFormat;
+import java.io.File;
 import org.json.JSONObject;
 import org.ligi.axt.AXT;
 import org.ligi.passandroid.helper.SafeJSONReader;
+import org.ligi.passandroid.model.BarCode;
 import org.ligi.passandroid.model.FiledPass;
 import org.ligi.passandroid.model.PassImpl;
 import org.ligi.tracedroid.logging.Log;
-
-import java.io.File;
 
 public class PassReader {
 
@@ -23,7 +23,6 @@ public class PassReader {
         pass.setPath(path);
 
         pass.setId(path.substring(path.lastIndexOf('/') + 1));
-
         final File file = new File(path + "/data.json");
 
         try {
@@ -34,6 +33,20 @@ public class PassReader {
             pass.setType(pass_json.getString("type"));
             pass.setBackgroundColor(Color.parseColor(pass_json.getString("bgColor")));
             pass.setForegroundColor(Color.parseColor(pass_json.getString("fgColor")));
+
+            if (pass_json.has("barcode")) {
+                final JSONObject barcode_json = pass_json.getJSONObject("barcode");
+                final String barcodeFormatString = barcode_json.getString("type");
+
+                final BarcodeFormat barcodeFormat = BarCode.getFormatFromString(barcodeFormatString);
+                final BarCode barCode = new BarCode(barcodeFormat, barcode_json.getString("message"));
+                pass.setBarCode(barCode);
+
+                if (barcode_json.has("altText")) {
+                    barCode.setAlternativeText(barcode_json.getString("altText"));
+                }
+
+            }
 
         } catch (Exception e) {
             Log.i("PassParse Exception " + e);
