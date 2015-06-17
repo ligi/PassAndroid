cdd0bbf2ce58dd002bc180fcdb12e8bad1b9c8f9
ligi <ligi@ligi.de>
2015-06-18 00:16:57 +0200

    We need to click on BarCode here - swipe does not work on snap-ci

--- src/androidTest/java/org/ligi/passandroid/steps/PassEditSteps.java
+++ src/androidTest/java/org/ligi/passandroid/steps/PassEditSteps.java
@@ -3,8 +3,10 @@ package org.ligi.passandroid.steps;
 import org.ligi.passandroid.R;
 
 import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.swipeLeft;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 public class PassEditSteps {
     public static void goToMetaData() {
@@ -25,6 +27,6 @@ public class PassEditSteps {
 
     public static void goToBarCode() {
         goToColor();
-        onView(withId(R.id.passEditPager)).perform(swipeLeft());
+        onView(withText("BarCode")).perform(click());
     }
 }
