321c81c1227142293f43cac77f7fc2c1f8522570
ligi <ligi@ligi.de>
2015-09-10 13:24:21 +0200

    Unfortunately we need to add a sleep here for the new ViewPager :-(

--- src/androidTest/java/org/ligi/passandroid/steps/PassEditSteps.java
+++ src/androidTest/java/org/ligi/passandroid/steps/PassEditSteps.java
@@ -1,18 +1,14 @@
 package org.ligi.passandroid.steps;
 
 import org.ligi.passandroid.R;
-
 import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.action.ViewActions.swipeLeft;
-import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
 
 public class PassEditSteps {
     public static void goToMetaData() {
         onView(withId(R.id.passEditPager)).perform(swipeLeft());
+        sleep();
     }
 
     public static void goToImages() {
@@ -30,6 +26,16 @@ public class PassEditSteps {
     public static void goToBarCode() {
         goToColor();
         onView(withId(R.id.passEditPager)).perform(swipeLeft());
-        onView(withId(R.id.barcodeTypeRadioGroup)).check(matches(isDisplayed()));
+        sleep();
     }
+
+    private static void sleep() {
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+
 }
