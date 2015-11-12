ea6f29f146b93cf424ecbde3fae2a181dd749d12
ligi <ligi@ligi.de>
2015-11-12 16:13:37 +0100

    Rewrite URL for Brussels Airlines

--- android/src/main/java/org/ligi/passandroid/ui/quirk_fix/URLRewriteActivity.java
+++ android/src/main/java/org/ligi/passandroid/ui/quirk_fix/URLRewriteActivity.java
@@ -6,62 +6,72 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
-import java.net.URLEncoder;
+
 import org.ligi.passandroid.Tracker;
 import org.ligi.passandroid.ui.PassImportActivity;
 
+import java.net.URLEncoder;
+
 public class URLRewriteActivity extends Activity {
+
+    private String getUrlByHost(final String host) {
+        if (host.endsWith(".virginaustralia.com")) { // mobile. or checkin.
+            return getVirginAustraliaURL();
+        }
+
+        switch (host) {
+            case "www.cathaypacific.com":
+                return getCathay();
+            case "mbp.swiss.com":
+            case "prod.wap.ncrwebhost.mobi":
+                return getNrcWebHost();
+        }
+
+        return null;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        String url = null;
-
-        if (getIntent().getData().getHost().endsWith(".virginaustralia.com")) { // mobile. or checkin.
-            url = getVirginAustraliaURL();
-        } else if (getIntent().getData().getHost().equals("www.cathaypacific.com")) {
-            url = getCathay();
-        } else if (getIntent().getData().getHost().equals("mbp.swiss.com")) {
-            url = getSwiss();
-        }
+        final String url = getUrlByHost(getIntent().getData().getHost());
 
         if (url == null) {
             new AlertDialog.Builder(this).setTitle("Workaround failed")
-                                         .setMessage(
-                                                 "The URL PassAndroid tried to work around failed :-( some companies just send PassBooks to Apple Devices - this was an attempt to workaround this." +
-                                                 "Unfortunately it failed - perhaps there where changes on the serverside - you can open the site with your browser now - to see it in PassAndroid in future again it would help if you can send me the pass")
-                                         .setPositiveButton("Browser", new DialogInterface.OnClickListener() {
-                                             @Override
-                                             public void onClick(DialogInterface dialog, int which) {
-                                                 Tracker.get().trackException("URLRewrite with invalid activity", false);
-                                                 final Intent intent = new Intent(URLRewriteActivity.this, OpenIphoneWebView.class);
-                                                 intent.setData(getIntent().getData());
-                                                 startActivity(intent);
+                    .setMessage(
+                            "The URL PassAndroid tried to work around failed :-( some companies just send PassBooks to Apple Devices - this was an attempt to workaround this." +
+                                    "Unfortunately it failed - perhaps there where changes on the serverside - you can open the site with your browser now - to see it in PassAndroid in future again it would help if you can send me the pass")
+                    .setPositiveButton("Browser", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            Tracker.get().trackException("URLRewrite with invalid activity", false);
+                            final Intent intent = new Intent(URLRewriteActivity.this, OpenIphoneWebView.class);
+                            intent.setData(getIntent().getData());
+                            startActivity(intent);
 
-                                             }
-                                         })
-                                         .setNeutralButton("send", new DialogInterface.OnClickListener() {
-                                                               @Override
-                                                               public void onClick(DialogInterface dialog, int which) {
+                        }
+                    })
+                    .setNeutralButton("send", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
 
-                                                                   Intent intent = new Intent(Intent.ACTION_SEND);
-                                                                   intent.putExtra(Intent.EXTRA_SUBJECT, "PassAndroid: URLRewrite Problem");
-                                                                   intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"ligi@ligi.de"});
-                                                                   intent.putExtra(Intent.EXTRA_TEXT, getIntent().getData().toString());
-                                                                   intent.setType("text/plain");
+                            Intent intent = new Intent(Intent.ACTION_SEND);
+                            intent.putExtra(Intent.EXTRA_SUBJECT, "PassAndroid: URLRewrite Problem");
+                            intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"ligi@ligi.de"});
+                            intent.putExtra(Intent.EXTRA_TEXT, getIntent().getData().toString());
+                            intent.setType("text/plain");
 
-                                                                   startActivity(Intent.createChooser(intent, "How to send Link?"));
-                                                                   finish();
-                                                               }
-                                                           })
-                                         .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
-                                             @Override
-                                             public void onClick(DialogInterface dialog, int which) {
-                                                 URLRewriteActivity.this.finish();
-                                             }
-                                         })
-                                         .show();
+                            startActivity(Intent.createChooser(intent, "How to send Link?"));
+                            finish();
+                        }
+                    })
+                    .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            URLRewriteActivity.this.finish();
+                        }
+                    })
+                    .show();
 
             return;
         }
@@ -72,8 +82,11 @@ public class URLRewriteActivity extends Activity {
         finish();
     }
 
-    private String getVirginAustraliaURL() {
+    private String getBrusselsAirline() {
+        return "";
+    }
 
+    private String getVirginAustraliaURL() {
 
         final Uri data = getIntent().getData();
 
@@ -109,11 +122,10 @@ public class URLRewriteActivity extends Activity {
         return "https://www.cathaypacific.com/icheckin2/PassbookServlet?v=" + URLEncoder.encode(passId);
     }
 
-    public String getSwiss() {
+    public String getNrcWebHost() {
         String url = getIntent().getData().toString();
         if (url.endsWith("/")) {
             url = url.substring(0, url.length() - 1);
-
         }
 
         final String[] split = url.split("/");
