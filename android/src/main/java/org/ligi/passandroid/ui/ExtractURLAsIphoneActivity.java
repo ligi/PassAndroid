d6df268ea5c2546ea80237eda82c3d26c83d77ac
ligi <ligi@ligi.de>
2016-04-17 16:15:33 +0200

    Adapt to server-side changes of Virgin-Australia

--- android/src/main/java/org/ligi/passandroid/ui/ExtractURLAsIphoneActivity.java
+++ android/src/main/java/org/ligi/passandroid/ui/ExtractURLAsIphoneActivity.java
@@ -5,18 +5,16 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.Bundle;
-
+import android.support.annotation.Nullable;
 import com.squareup.okhttp.OkHttpClient;
 import com.squareup.okhttp.Request;
 import com.squareup.okhttp.ResponseBody;
-
-import org.ligi.passandroid.ui.quirk_fix.OpenIphoneWebView;
-
 import java.io.IOException;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.ligi.passandroid.ui.quirk_fix.OpenIphoneWebView;
 
 public class ExtractURLAsIphoneActivity extends PassAndroidActivity {
 
@@ -46,13 +44,12 @@ public class ExtractURLAsIphoneActivity extends PassAndroidActivity {
                 final ResponseBody body = client.newCall(requestBuilder.build()).execute().body();
                 final String bodyString = body.string();
                 body.close();
-                final Pattern p = Pattern.compile("href=\"(.*\\.pkpass.*?)\"");
-                final Matcher m = p.matcher(bodyString);
 
-                if (!m.find()) {
+                final String url = extractURL(bodyString);
+
+                if (url == null) {
                     return null;
                 }
-                final String url = m.group(1); // this variable should contain the link URL
 
                 if (!url.startsWith("http")) {
                     return getIntent().getData().getScheme() + "://" + getIntent().getData().getHost() + "/" + url;
@@ -64,6 +61,22 @@ public class ExtractURLAsIphoneActivity extends PassAndroidActivity {
             }
 
             return null;
+        }
+
+        @Nullable
+        private String extractURL(String body) {
+            final String[] patterns = new String[] { "href=\"(.*\\.pkpass.*?)\"" , "window.location = \'(.*\\.pkpass.*?)\'"};
+
+            for (final String pattern : patterns) {
+                final Pattern p = Pattern.compile(pattern);
+                final Matcher m = p.matcher(body);
+
+                if (m.find()) {
+                    return m.group(1);
+                }
+            }
+
+            return null ;
         }
 
         @Override
