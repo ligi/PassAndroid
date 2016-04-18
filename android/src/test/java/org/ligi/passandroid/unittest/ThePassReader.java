5a1c1145de885d8e486754f538685eb79ec79bac
ligi <ligi@ligi.de>
2016-04-18 02:04:33 +0200

    Improve naming

--- android/src/test/java/org/ligi/passandroid/unittest/ThePassReader.java
+++ android/src/test/java/org/ligi/passandroid/unittest/ThePassReader.java
@@ -2,22 +2,18 @@ package org.ligi.passandroid.unittest;
 
 import com.squareup.moshi.JsonAdapter;
 import com.squareup.moshi.Moshi;
-
-import org.junit.Test;
-import org.ligi.axt.AXT;
-
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.URISyntaxException;
-
+import org.junit.Test;
+import org.ligi.axt.AXT;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class ThePassReader {
 
     @Test
-    public void foo() throws URISyntaxException, IOException {
+    public void testMinimalOpass2() throws URISyntaxException, IOException {
         final FileInputStream fileInputStream = new FileInputStream(new File(getClass().getClassLoader()
                 .getResource("opass_v2/minimal.json")
                 .toURI()));
