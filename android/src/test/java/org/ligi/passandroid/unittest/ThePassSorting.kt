9813c9b1d952debd0c3a6baef00fbb0eadb7951e
ligi <ligi@ligi.de>
2016-09-10 14:16:49 +0200

    Add tests to cover pass sorting / comparators

---
+++ android/src/test/java/org/ligi/passandroid/unittest/ThePassSorting.kt
@@ -0,0 +1,64 @@
+package org.ligi.passandroid.unittest
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.Before
+import org.junit.Test
+import org.ligi.passandroid.model.comparator.PassSortOrder
+import org.ligi.passandroid.model.pass.Pass
+import org.ligi.passandroid.model.pass.PassImpl
+import org.ligi.passandroid.model.pass.PassType
+import org.threeten.bp.ZonedDateTime
+import java.util.*
+
+class ThePassSorting {
+
+    private val pass1 = PassImpl("ID1")
+    private val pass2 = PassImpl("ID2")
+    private val pass3 = PassImpl("ID3")
+    private val pass4 = PassImpl("ID3")
+
+    lateinit private var passList: List<Pass>
+
+    @Before
+    fun init() {
+        pass1.calendarTimespan = PassImpl.TimeSpan(ZonedDateTime.now().minusHours(5))
+        pass3.calendarTimespan = PassImpl.TimeSpan(ZonedDateTime.now().plusHours(1))
+        pass2.calendarTimespan = PassImpl.TimeSpan(ZonedDateTime.now().plusHours(2))
+
+        pass1.type = PassType.GENERIC
+        pass2.type = PassType.EVENT
+        pass3.type = PassType.GENERIC
+        pass4.type = PassType.GENERIC
+
+        passList = mutableListOf(pass1, pass2, pass3, pass4)
+    }
+
+    @Test
+    fun testDESC() {
+        Collections.sort(passList, PassSortOrder.DATE_DESC.toComparator())
+
+        assertThat(passList).containsExactly(pass4, pass1, pass3, pass2)
+    }
+
+    @Test
+    fun testASC() {
+        Collections.sort(passList, PassSortOrder.DATE_ASC.toComparator())
+
+        assertThat(passList).containsExactly(pass2, pass3, pass1, pass4)
+    }
+
+    @Test
+    fun testDIFF() {
+        Collections.sort(passList, PassSortOrder.DATE_DIFF.toComparator())
+
+        assertThat(passList).containsExactly(pass3, pass2, pass1, pass4)
+    }
+
+    @Test
+    fun testTYPE() {
+        Collections.sort(passList, PassSortOrder.TYPE.toComparator())
+
+        assertThat(passList).containsExactly(pass3, pass1, pass4, pass2)
+    }
+
+}
