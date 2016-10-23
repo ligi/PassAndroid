e4e49f89c407605b4635e14b73a0550efa694f59
ligi <ligi@ligi.de>
2016-10-23 17:20:47 +0200

    Improve help code

---
+++ android/src/main/java/org/ligi/passandroid/ui/HelpActivity.kt
@@ -0,0 +1,46 @@
+package org.ligi.passandroid.ui
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.text.Editable
+import android.text.Html
+import android.text.method.LinkMovementMethod
+import android.view.MenuItem
+import kotlinx.android.synthetic.main.activity_help.*
+import org.ligi.passandroid.BuildConfig
+import org.ligi.passandroid.R
+import org.xml.sax.XMLReader
+
+class HelpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_help)
+
+        val html = Html.fromHtml(getString(R.string.help_content), null, ListTagHandler())
+
+        help_text.text = html
+        help_text.movementMethod = LinkMovementMethod()
+
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.subtitle = "v" + BuildConfig.VERSION_NAME
+    }
+
+    internal inner class ListTagHandler : Html.TagHandler {
+
+        override fun handleTag(opening: Boolean, tag: String, output: Editable, xmlReader: XMLReader) {
+            if (tag.equals("li", ignoreCase = true)) {
+                output.append(if (opening) "\u2022 " else "\n")
+            }
+        }
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
+        android.R.id.home -> {
+            finish()
+            true
+        }
+        else -> super.onOptionsItemSelected(item)
+    }
+}
