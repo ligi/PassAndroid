6a7c45e8aef6964f4166dd1a34b191f22e8b6c8a
ligi <ligi@ligi.de>
2016-10-29 18:42:19 +0200

    Add new action in NavDrawer to send people to transifex and improve navigation-code

--- android/src/main/java/org/ligi/passandroid/ui/PassNavigationView.kt
+++ android/src/main/java/org/ligi/passandroid/ui/PassNavigationView.kt
@@ -1,15 +1,14 @@
 package org.ligi.passandroid.ui
 
 import android.content.Context
-import android.support.annotation.LayoutRes
+import android.content.Intent
+import android.net.Uri
 import android.support.design.widget.NavigationView
 import android.util.AttributeSet
-import android.view.View
 import kotlinx.android.synthetic.main.navigation_drawer_header.view.*
 import org.greenrobot.eventbus.EventBus
 import org.greenrobot.eventbus.Subscribe
 import org.greenrobot.eventbus.ThreadMode
-import org.ligi.axt.AXT
 import org.ligi.passandroid.App
 import org.ligi.passandroid.R
 import org.ligi.passandroid.events.PassStoreChangeEvent
@@ -24,48 +23,36 @@ class PassNavigationView(context: Context, attrs: AttributeSet) : NavigationView
     @Inject
     lateinit internal var bus: EventBus
 
-    init {
-
-        setNavigationItemSelectedListener(OnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.menu_plus -> {
-                    AXT.at(getContext()).startCommonIntent().openUrl("https://plus.google.com/communities/116353894782342292067")
-                    return@OnNavigationItemSelectedListener true
-                }
-
-                R.id.menu_github -> {
-                    AXT.at(getContext()).startCommonIntent().openUrl("https://github.com/ligi/PassAndroid")
-                    return@OnNavigationItemSelectedListener true
-                }
-
-                R.id.menu_share -> {
-                    AXT.at(getContext()).startCommonIntent().shareUrl(marketUrl)
-                    return@OnNavigationItemSelectedListener true
-                }
-
-                R.id.menu_beta -> {
-                    AXT.at(getContext()).startCommonIntent().openUrl("https://play.google.com/apps/testing/org.ligi.passandroid")
-                    return@OnNavigationItemSelectedListener true
-                }
-
-                R.id.menu_settings -> {
-                    AXT.at(getContext()).startCommonIntent().activityFromClass(PreferenceActivity::class.java)
-                    return@OnNavigationItemSelectedListener true
-                }
-            }
-
-            false
-        })
+    fun getIntent(id: Int) = when (id) {
+        R.id.menu_settings -> Intent(context, PreferenceActivity::class.java)
+        R.id.menu_plus -> intentFromUrl("https://plus.google.com/communities/116353894782342292067")
+        R.id.menu_github -> intentFromUrl("https://github.com/ligi/PassAndroid")
+        R.id.menu_beta -> intentFromUrl("https://play.google.com/apps/testing/org.ligi.passandroid")
+        R.id.menu_language -> intentFromUrl("https://transifex.com/projects/p/passandroid")
+        R.id.menu_share -> Intent(Intent.ACTION_SEND).apply {
+            putExtra(Intent.EXTRA_TEXT, marketUrl)
+            type = "text/plain"
+        }
+        else -> null
     }
 
-    override fun inflateHeaderView(@LayoutRes res: Int): View {
-        val view = super.inflateHeaderView(res)
+    fun intentFromUrl(url: String) = Intent(Intent.ACTION_VIEW).apply { data = Uri.parse(url) }
+
+    override fun onAttachedToWindow() {
+        super.onAttachedToWindow()
 
         App.component().inject(this)
 
         bus.register(this)
+
+        setNavigationItemSelectedListener { item ->
+            getIntent(item.itemId)?.let {
+                context.startActivity(it)
+                true
+            } ?: false
+        }
+
         onPassStoreChangeEvent(PassStoreChangeEvent)
-        return view
     }
 
     override fun onDetachedFromWindow() {
@@ -73,11 +60,10 @@ class PassNavigationView(context: Context, attrs: AttributeSet) : NavigationView
         bus.unregister(this)
     }
 
-    private val marketUrl: String
-        get() = context.getString(R.string.market_url, context.packageName)
+    private val marketUrl by lazy { context.getString(R.string.market_url, context.packageName) }
 
     @Subscribe(threadMode = ThreadMode.MAIN)
-    fun onPassStoreChangeEvent(passStoreChangeEvent: PassStoreChangeEvent) {
+    fun onPassStoreChangeEvent(@Suppress("UNUSED_PARAMETER") passStoreChangeEvent: PassStoreChangeEvent) {
 
         val passCount = passStore.passMap.size
         getHeaderView(0).pass_count_header.text = context.getString(R.string.passes_nav, passCount)
