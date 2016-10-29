7273da05cb10e146d68be06038147bc0b60271ea
ligi <ligi@ligi.de>
2016-10-29 19:20:30 +0200

    Add tests for navitgation-drawer

--- android/src/androidTest/java/org/ligi/passandroid/ThePassListActivity.java
+++ android/src/androidTest/java/org/ligi/passandroid/ThePassListActivity.java
@@ -9,10 +9,8 @@ import org.ligi.passandroid.ui.PassListActivity;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
-import static android.support.test.espresso.contrib.DrawerActions.open;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static org.hamcrest.CoreMatchers.not;
 import static org.ligi.passandroid.steps.HelpSteps.checkThatHelpIsThere;
 
 @TargetApi(14)
@@ -33,21 +31,6 @@ public class ThePassListActivity {
         onView(withId(R.id.menu_help)).perform(click());
 
         checkThatHelpIsThere();
-    }
-
-    @Test
-    public void testNavigationDrawerIsUsuallyNotShown() {
-        onView(withId(R.id.navigationView)).check(matches(not(isDisplayed())));
-    }
-
-
-    @Test
-    public void testThatNavigationDrawerOpens() {
-
-        onView(withId(R.id.drawer_layout)).perform(open());
-        onView(withId(R.id.navigationView)).check(matches(isDisplayed()));
-
-        Spoon.screenshot(rule.getActivity(), "open_drawer");
     }
 
 
