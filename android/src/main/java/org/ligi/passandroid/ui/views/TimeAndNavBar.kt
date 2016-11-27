582a1519bf0a6de61af362cb611149f7fb66beb0
ligi <ligi@ligi.de>
2016-11-27 16:51:54 +0100

    Replace clickable_bg and Improve TimeAndNavBar

--- android/src/main/java/org/ligi/passandroid/ui/views/TimeAndNavBar.kt
+++ android/src/main/java/org/ligi/passandroid/ui/views/TimeAndNavBar.kt
@@ -3,49 +3,25 @@ package org.ligi.passandroid.ui.views
 import android.content.Context
 import android.support.v7.widget.AppCompatDrawableManager
 import android.util.AttributeSet
-import android.view.Gravity
-import android.view.ViewGroup
-import android.widget.LinearLayout
+import android.view.LayoutInflater
+import android.widget.FrameLayout
 import android.widget.TextView
 import org.ligi.passandroid.R
 
-open class TimeAndNavBar constructor(context: Context, attrs: AttributeSet) : LinearLayout(context, attrs) {
+class TimeAndNavBar constructor(context: Context, attrs: AttributeSet) : FrameLayout(context, attrs) {
 
-    private fun baseTextView(context: Context) = TextView(context).apply {
-        layoutParams = weightLayoutParams
-        val dimension = context.resources.getDimension(R.dimen.rhythm).toInt()
-        setPadding(dimension, dimension, dimension, dimension)
-        isClickable = true
-        setBackgroundResource(R.drawable.clickable_bg)
-    }
+    val timeTextView by lazy { findViewById(R.id.timeButton) as TextView }
+    val navTextView by lazy { findViewById(R.id.locationButton) as TextView }
 
-    val navTextView by lazy {
-        baseTextView(context)
-    }
+    init {
+        LayoutInflater.from(context).inflate(R.layout.time_and_nav, this)
+        AppCompatDrawableManager.get().apply {
+            val navDrawable = getDrawable(context, R.drawable.ic_maps_place)
+            navTextView.setCompoundDrawablesWithIntrinsicBounds(navDrawable, null, null, null)
 
-
-    val timeTextView by lazy {
-        baseTextView(context).apply {
-            gravity = Gravity.RIGHT
+            val timeDrawable = getDrawable(context, R.drawable.ic_action_today)
+            timeTextView.setCompoundDrawablesWithIntrinsicBounds(null, null, timeDrawable, null)
         }
-    }
-
-    val weightLayoutParams by lazy {
-        LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT).apply {
-            weight = 1.toFloat()
-        }
-    }
-
-    override fun onFinishInflate() {
-        super.onFinishInflate()
-        addView(navTextView)
-        addView(timeTextView)
-
-        val drawableManager = AppCompatDrawableManager.get()
-        val navDrawable = drawableManager.getDrawable(context, R.drawable.ic_maps_place)
-        navTextView.setCompoundDrawablesWithIntrinsicBounds(navDrawable, null, null, null)
-        val timeDrawable = drawableManager.getDrawable(context, R.drawable.ic_action_today)
-        timeTextView.setCompoundDrawablesWithIntrinsicBounds(null, null, timeDrawable, null)
     }
 
 }
