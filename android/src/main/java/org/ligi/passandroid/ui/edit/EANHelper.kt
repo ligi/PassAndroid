387e5f76bdb2b8f225c4d6bf01abbedbf238ff05
ligi <ligi@ligi.de>
2016-12-24 12:37:47 +0100

    Simplify code

--- android/src/main/java/org/ligi/passandroid/ui/edit/EANHelper.kt
+++ android/src/main/java/org/ligi/passandroid/ui/edit/EANHelper.kt
@@ -3,8 +3,8 @@ package org.ligi.passandroid.ui.edit
 import com.google.zxing.oned.EAN13Writer
 
 fun getRandomEAN13(): String {
-    val randomString = 0.rangeTo(11).map { (Math.random() * 9).toInt().toString() }.joinToString(separator = "") { it }
-    return 0.rangeTo(9).map { randomString + it }.first(::isValidEAN13)
+    val randomString = (0..11).map { (Math.random() * 9).toInt() }.joinToString(separator = "")
+    return (0..9).map { randomString + it }.first(::isValidEAN13)
 }
 
 fun isValidEAN13(payload: String) = try {
