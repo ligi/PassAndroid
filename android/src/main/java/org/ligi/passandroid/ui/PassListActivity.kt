1f4ac8bd7bd8dcc516dbdaac4bded59fe04c6767
Simon Tenbeitel <simon.tenbeitel@sap.com>
2017-02-28 17:34:34 +0100

    Close FAB menu on back pressed

--- android/src/main/java/org/ligi/passandroid/ui/PassListActivity.kt
+++ android/src/main/java/org/ligi/passandroid/ui/PassListActivity.kt
@@ -302,4 +302,9 @@ class PassListActivity : PassAndroidActivity() {
 
     }
 
+    override fun onBackPressed() {
+        if (fam.isExpanded) fam.collapse()
+        else super.onBackPressed()
+    }
+
 }
