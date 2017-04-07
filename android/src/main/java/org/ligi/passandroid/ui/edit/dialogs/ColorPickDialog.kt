ac8a7339cd536b533945fb7a2706aa4d120b4cd2
ligi <ligi@ligi.de>
2017-04-07 19:40:34 +0200

    Remove unused parameters

--- android/src/main/java/org/ligi/passandroid/ui/edit/dialogs/ColorPickDialog.kt
+++ android/src/main/java/org/ligi/passandroid/ui/edit/dialogs/ColorPickDialog.kt
@@ -14,7 +14,7 @@ fun showColorPickDialog(context: Context, pass: Pass, bus: EventBus) {
 
     inflate.colorPicker.color = pass.accentColor
     inflate.colorPicker.oldCenterColor = pass.accentColor
-    AlertDialog.Builder(context).setView(inflate).setPositiveButton(android.R.string.ok) { dialog, which ->
+    AlertDialog.Builder(context).setView(inflate).setPositiveButton(android.R.string.ok) { _, _ ->
         pass.accentColor = inflate.colorPicker.color
         bus.post(PassRefreshEvent(pass))
     }.setNegativeButton(android.R.string.cancel, null).setTitle(R.string.change_color_dialog_title).show()
