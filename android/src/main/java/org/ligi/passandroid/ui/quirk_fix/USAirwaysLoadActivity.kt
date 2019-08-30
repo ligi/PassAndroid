d624383d910de3ab7e5909b78ee9a08942a40ede
Liuhao <lansedeyu94@live.com>
2019-08-30 11:01:52 +0800

    fixed some bug in the application and makes it looks DRY

--- android/src/main/java/org/ligi/passandroid/ui/quirk_fix/USAirwaysLoadActivity.kt
+++ android/src/main/java/org/ligi/passandroid/ui/quirk_fix/USAirwaysLoadActivity.kt
@@ -4,7 +4,10 @@ import android.content.DialogInterface
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AlertDialog
+import androidx.fragment.app.FragmentTransaction
+import org.ligi.passandroid.ui.AlertFragment
 
 import org.ligi.passandroid.ui.PassAndroidActivity
 import org.ligi.passandroid.ui.PassImportActivity
@@ -15,29 +18,9 @@ class USAirwaysLoadActivity : PassAndroidActivity() {
         super.onCreate(savedInstanceState)
 
         val data = intent.data
-        if (data == null){
-            AlertDialog.Builder(this).setTitle("Workaround failed")
-                    .setMessage(
-                            "The URL PassAndroid tried to work around failed :-( some companies just send PassBooks to Apple Devices - this was an attempt to workaround this." + "Unfortunately it failed - perhaps there where changes on the serverside - you can open the site with your browser now - to see it in PassAndroid in future again it would help if you can send me the pass")
-                    .setPositiveButton("Browser") { dialog, which ->
-                        tracker.trackException("URLRewrite with invalid activity", false)
-                        val intent = Intent(this@USAirwaysLoadActivity, OpenIphoneWebView::class.java)
-                        intent.data = data
-                        startActivity(intent)
-                    }
-                    .setNeutralButton("send") { dialog, which ->
-                        val intent = Intent(Intent.ACTION_SEND)
-                        intent.putExtra(Intent.EXTRA_SUBJECT, "PassAndroid: URLRewrite Problem")
-                        intent.putExtra(Intent.EXTRA_EMAIL, arrayOf("ligi@ligi.de"))
-                        intent.putExtra(Intent.EXTRA_TEXT, data.toString())
-                        intent.type = "text/plain"
-
-                        startActivity(Intent.createChooser(intent, "How to send Link?"))
-                        finish()
-                    }
-                    .setNegativeButton("cancel") { dialog, which -> this@USAirwaysLoadActivity.finish() }
-                    .show()
-
+        if (data == null || data.toString().indexOf("/") == -1){
+            val alert = AlertFragment()
+            supportFragmentManager.beginTransaction().add(alert,"AlertFrag").commit()
             return
         }
         val url = data?.toString()?.removeSuffix("/") ?: ""
