21230a8afe4a3dc89f8329624a3f13640c6d2e11
gaurav <gb@bajaj-code.de>
2024-09-26 17:38:00 +0200

    Updated a few things so that the app works under current android architecture

--- android/src/main/java/org/ligi/passandroid/ui/PassListActivity.kt
+++ android/src/main/java/org/ligi/passandroid/ui/PassListActivity.kt
@@ -12,9 +12,13 @@ import android.view.Menu
 import android.view.MenuItem
 import android.view.View.GONE
 import android.view.View.VISIBLE
+import android.view.ViewGroup
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
 import androidx.core.view.GravityCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
 import androidx.lifecycle.lifecycleScope
 import androidx.viewpager.widget.ViewPager
 import com.google.android.material.snackbar.Snackbar
@@ -107,6 +111,24 @@ class PassListActivity : PassAndroidActivity() {
         }
 
         binding.drawerLayout.addDrawerListener(drawerToggle)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.navigationView){
+            v, windowInsets ->
+            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            // Apply the insets as a margin to the view. This solution sets
+            // only the bottom, left, and right dimensions, but you can apply whichever
+            // insets are appropriate to your layout. You can also update the view padding
+            // if that's more appropriate.
+            v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                leftMargin = insets.left
+                bottomMargin = insets.bottom
+                rightMargin = insets.right
+                topMargin = insets.top
+            }
+
+            // Return CONSUMED if you don't want want the window insets to keep passing
+            // down to descendant views.
+            WindowInsetsCompat.CONSUMED
+        }
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
--- android/src/main/java/org/ligi/passandroid/ui/PreferenceActivity.kt
+++ android/src/main/java/org/ligi/passandroid/ui/PreferenceActivity.kt
@@ -1,9 +1,17 @@
 package org.ligi.passandroid.ui
 
 import android.annotation.SuppressLint
+import android.content.Context
 import android.os.Bundle
+import android.util.AttributeSet
+import android.view.LayoutInflater
 import androidx.appcompat.app.AppCompatActivity
 import android.view.MenuItem
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
 import org.ligi.passandroid.R
 
 class PreferenceActivity : AppCompatActivity() {
@@ -13,6 +21,26 @@ class PreferenceActivity : AppCompatActivity() {
 
         setContentView(R.layout.preferences)
 
+        val prefs: View? = findViewById(android.R.id.content)
+        if (prefs != null) {
+            ViewCompat.setOnApplyWindowInsetsListener(prefs){ v, windowInsets ->
+                val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+                // Apply the insets as a margin to the view. This solution sets
+                // only the bottom, left, and right dimensions, but you can apply whichever
+                // insets are appropriate to your layout. You can also update the view padding
+                // if that's more appropriate.
+                v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                    topMargin = insets.top
+                    leftMargin = insets.left
+                    bottomMargin = insets.bottom
+                    rightMargin = insets.right
+                }
+
+                // Return CONSUMED if you don't want want the window insets to keep passing
+                // down to descendant views.
+                WindowInsetsCompat.CONSUMED
+            }
+        }
         supportActionBar?.setDisplayShowTitleEnabled(true)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
@@ -27,3 +55,5 @@ class PreferenceActivity : AppCompatActivity() {
     }
 
 }
+
+
