43779339034b35936896b4f661084305761b80a1
ligi <ligi@ligi.de>
2024-10-20 16:02:42 +0200

    Update android/src/main/java/org/ligi/passandroid/ui/PreferenceActivity.kt

--- android/src/main/java/org/ligi/passandroid/ui/PreferenceActivity.kt
+++ android/src/main/java/org/ligi/passandroid/ui/PreferenceActivity.kt
@@ -21,26 +21,6 @@ class PreferenceActivity : AppCompatActivity() {
 
         setContentView(R.layout.preferences)
 
-        val prefs: View? = findViewById(android.R.id.content)
-        if (prefs != null) {
-            ViewCompat.setOnApplyWindowInsetsListener(prefs){ v, windowInsets ->
-                val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
-                // Apply the insets as a margin to the view. This solution sets
-                // only the bottom, left, and right dimensions, but you can apply whichever
-                // insets are appropriate to your layout. You can also update the view padding
-                // if that's more appropriate.
-                v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
-                    topMargin = insets.top
-                    leftMargin = insets.left
-                    bottomMargin = insets.bottom
-                    rightMargin = insets.right
-                }
-
-                // Return CONSUMED if you don't want want the window insets to keep passing
-                // down to descendant views.
-                WindowInsetsCompat.CONSUMED
-            }
-        }
         supportActionBar?.setDisplayShowTitleEnabled(true)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
