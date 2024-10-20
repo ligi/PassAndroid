978448e6d619fda95d73c1bfeceb86cc8bf4ec33
ligi <ligi@ligi.de>
2024-10-20 16:02:32 +0200

    Update android/src/main/java/org/ligi/passandroid/ui/PassListActivity.kt

--- android/src/main/java/org/ligi/passandroid/ui/PassListActivity.kt
+++ android/src/main/java/org/ligi/passandroid/ui/PassListActivity.kt
@@ -111,24 +111,6 @@ class PassListActivity : PassAndroidActivity() {
         }
 
         binding.drawerLayout.addDrawerListener(drawerToggle)
-        ViewCompat.setOnApplyWindowInsetsListener(binding.navigationView){
-            v, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars())
-            // Apply the insets as a margin to the view. This solution sets
-            // only the bottom, left, and right dimensions, but you can apply whichever
-            // insets are appropriate to your layout. You can also update the view padding
-            // if that's more appropriate.
-            v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
-                leftMargin = insets.left
-                bottomMargin = insets.bottom
-                rightMargin = insets.right
-                topMargin = insets.top
-            }
-
-            // Return CONSUMED if you don't want want the window insets to keep passing
-            // down to descendant views.
-            WindowInsetsCompat.CONSUMED
-        }
 
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
